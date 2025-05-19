0f9859536bfb621eb0fcf7e3a84b99a83b6ff628
Haruyuki Tanukiji <haruyuki@tanu.ch>
2025-05-19 18:17:31 +0800

    Don't include base kumoha

--- vite.config.ts
+++ vite.config.ts
@@ -19,7 +19,7 @@ export default defineConfig({
       formats: ['es', 'cjs']
     },
     rollupOptions: {
-      external: ['react', 'react-dom', 'opentetsu']
+      external: ['react', 'react-dom', 'opentetsu', '@tanuden/kumoha']
     }
   }
 });
